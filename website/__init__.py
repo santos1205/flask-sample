341e082304aa34c74ff0486a31c48e5658922d43
MARIO SANTOS <mariosantos1205@gmail.com>
2021-06-18 16:04:08 -0300

    sql alchemy config

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from .views import views
 from .auth import auth
+from flask import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
 
 def create_app():
-  app = Flask(__name__)
-  app.config['SECRET_KEY'] = 'AJFASKLJFADLSKFJASLD ADLKFJSDAÇLKFJDSFLKDJ dfljasçdljfaslfdçkj'
-  app.register_blueprint(views, url_prefix='/')
-  app.register_blueprint(auth, url_prefix='/')
-
-  return app
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'AJFASKLJFADLSKFJASLD ADLKFJSDAÇLKFJDSFLKDJ'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    return app
